5900d5318addd53db3424594411f89eac9621c2f
Wellington Ferreira da Silva <108769429+Wellingt0ndev@users.noreply.github.com>
2024-03-22 17:25:00 -0300

    Created project and Created the Position class

---
+++ Chess-console/Program.cs
@@ -0,0 +1,17 @@
+﻿using Chess_console.Board;
+
+namespace Chess_console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Position p;
+
+            p = new Position(3, 4);
+
+            Console.WriteLine("Position: " +  p);
+             
+        }
+    }
+}
